61371c423661a05d58bb4d700ed93d03ab742b05
delan azabani <dazabani@igalia.com>
2026-03-12 16:31:45 +0800

    Run `git show` in tmux when pressing `S`
    requires tmux in PATH, but using tmux is optional.
    
    to use tmux:
    
    $ tmux -L commit-triage new cargo run /path/to/commits.txt

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 use std::cell::Cell;
 use std::env;
 use std::io;
+use std::io::Write;
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use std::process::{Child, Command, Stdio};
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
@@ -33,6 +34,7 @@ pub struct App {
     /// Position of cursor in the editor area.
     byte_index: usize,
     cursor_pos: Cell<Option<(u16, u16)>>,
+    tmux: Child,
 }
 
 fn main() {
@@ -53,10 +55,18 @@ fn main() {
         input: String::new(),
         byte_index: 0,
         cursor_pos: Cell::new(None),
+        tmux: Command::new("tmux")
+            .args(["-CL", "commit-triage", "attach-session"])
+            .stdin(Stdio::piped())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .unwrap(),
     };
     ratatui::run(|terminal| app.run(terminal)).unwrap();
 
     write_to_file(&app.commits, &app.path).unwrap();
+    app.tmux.kill().unwrap();
 }
 
 impl App {
@@ -103,6 +113,7 @@ impl App {
                 KeyCode::Char('-') => self.update_state(State::Ignored),
                 KeyCode::Char(' ') => self.unroll = !self.unroll,
                 KeyCode::Char('t') => self.open_tag_editor(),
+                KeyCode::Char('s') => self.git_show(),
                 _ => {}
             }
         } else {
@@ -251,6 +262,22 @@ impl App {
                     .spawn()
             });
         let _ = child.unwrap().wait();
+    }
+
+    fn git_show(&mut self) {
+        let command = format!(
+            // show with stat (lines changed), summary (files created and deleted), and patch.
+            // always run the pager, even if the output is short enough to not need scrolling
+            // (git’s default is LESS=FRX, which exits immediately for short outputs).
+            "new-window env LESS=RX git -C ~/servo show --stat --summary -p {}\n",
+            self.commits[self.index].hash
+        );
+        self.tmux
+            .stdin
+            .as_mut()
+            .unwrap()
+            .write_all(command.as_bytes())
+            .unwrap();
     }
 
     fn exit(&mut self) {
